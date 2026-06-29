2042cd4f506ebfc35f916e0ca69048b99cba0f9f
Piero <24100634@ue.edu.pe>
2026-06-28 21:44:20 -0500

    fix memory crash

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://labs15c-kum941qfh-ados-projects-0eb7f124.vercel.app/',
+    baseUrl: 'http://localhost:3000/',
+    experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 1,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
